ea4fade832924e8c3c3ec3bfe5e19819c2c1a00e
BlackIsDevin <58799343+BlackIsDevin@users.noreply.github.com>
2021-06-30 23:31:25 -0400

    added placeholder for CoreDatapath module

---
+++ CustomRiscV.src/Modules/CentralCore/CoreDatapath.v
@@ -0,0 +1,8 @@
+// this modules holds the central datapath of the CPU core, as well as exposes 
+// parts of data memory (and in the future, a writable instruction memory
+// module) to other modules
+module CoreDatapath(
+
+);
+    // TODO: literally all of this
+endmodule
