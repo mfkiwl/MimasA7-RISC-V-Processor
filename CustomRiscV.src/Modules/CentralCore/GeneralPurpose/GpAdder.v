4dddcfbb68a96b9e91434d29f7fccdb3a5cbae3c
BlackIsDevin <58799343+BlackIsDevin@users.noreply.github.com>
2021-07-10 02:19:14 -0400

    changed 64-bit adder into configurable width adder

--- CustomRiscV.src/Modules/CentralCore/GeneralPurpose/GpAdder.v
+++ CustomRiscV.src/Modules/CentralCore/GeneralPurpose/GpAdder.v
@@ -1,18 +1,20 @@
 /**
-    File: Gp64BitAdder.v
+    File: GpAdder.v
     Author: BlackIsDevin (https://github.com/BlackIsDevin)
     Date: 6/30/2021
     Target Devices:
         Mimas A7 Revision V3 Development Board
         Arty A7-35T Development Board (future target)
     Description:
-        This module implements a general-purpose 64-bit adder.
+        This module implements a general-purpose adder with configurable width.
 */
-module Gp64BitAdder(
-    input [63:0] a,
-    input [63:0] b,
+module GpAdder #(
+    parameter WIDTH = 64
+) (
+    input [WIDTH - 1:0] a,
+    input [WIDTH - 1:0] b,
 
-    output [63:0] s
+    output [WIDTH - 1:0] s
 );
 
     assign s = a + b;
